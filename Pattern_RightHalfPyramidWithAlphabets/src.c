d7555310d34f03b20c29c9af495f0ddeebe04c19
Christy Rachel Philip <40349694+ChristyRachel@users.noreply.github.com>
2023-07-22 22:04:07 -0400

    Pattern with Right Half Pyramid with Alphabets
    Pattern with Right Half Pyramid with Alphabets

---
+++ Pattern_RightHalfPyramidWithAlphabets/src.c
@@ -0,0 +1,33 @@
+/*
+ * src.c
+ *
+ *  Created on: Jul. 21, 2023
+ *      Author: christy
+ */
+
+
+/*
+ * src.c
+ *
+ *  Created on: Jul. 21, 2023
+ *      Author: christy
+ */
+
+#include <stdio.h>
+
+int main(){
+    int row;
+    scanf("%d",&row);
+
+    for(int i=0;i<row;i++){
+        for(int j=0;j<=i;j++){
+            printf("%c",'A'+j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+
+
+
