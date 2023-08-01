20cda2b108f27b32a344a66431630bbee170aab6
Christy Rachel Philip <40349694+ChristyRachel@users.noreply.github.com>
2023-08-01 13:54:57 -0400

    Reverse and print the input array
    Reverse and print the input array

---
+++ HR_C_ArrayReversal/src.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num, *arr, i;
+    scanf("%d", &num);
+    arr = (int*) malloc(num * sizeof(int));
+    for(i = 0; i < num; i++) {
+        scanf("%d", arr + i);
+    }
+
+    for(int j = num-1; j<(arr + j); j--)
+        printf("%d ", *(arr + j));
+    return 0;
+}
