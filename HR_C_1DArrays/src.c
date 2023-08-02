cb8e36b2a48c9f8d8c56290c592c9fd5fc2281fc
Christy Rachel Philip <40349694+ChristyRachel@users.noreply.github.com>
2023-08-02 10:02:16 -0400

    1 D Arrays in C
    Problem link: https://www.hackerrank.com/challenges/1d-arrays-in-c/problem?isFullScreen=true

---
+++ HR_C_1DArrays/src.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+
+    int n,sum=0;
+    scanf("%d\n",&n);
+
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+        sum+=arr[i];
+    }
+
+    printf("%d",sum);
+
+    return 0;
+}
