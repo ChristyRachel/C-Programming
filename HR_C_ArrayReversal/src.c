3e88b9c8b38b4b5bc9e3064178f85cdd8f572b95
Christy Rachel Philip <40349694+ChristyRachel@users.noreply.github.com>
2023-08-05 23:32:53 -0400

    src.c

--- HR_C_ArrayReversal/src.c
+++ HR_C_ArrayReversal/src.c
@@ -6,6 +6,7 @@ int main()
     int num, *arr, i;
     scanf("%d", &num);
     arr = (int*) malloc(num * sizeof(int));
+    // malloc : memory allocation for the array
     for(i = 0; i < num; i++) {
         scanf("%d", arr + i);
     }
